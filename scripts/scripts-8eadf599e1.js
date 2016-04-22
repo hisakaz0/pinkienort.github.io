4b272ca0b02dbd541ee4c7f98e0ee09d3c8ead49
pinkienort <cantabilehisa@gmail.com>
2016-04-22 16:15:35 +0900

    reset

--- scripts/scripts-8eadf599e1.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("ssorApp",["ngAnimate","ngAria","ngCookies","ngMessages","ngResource","ngRoute","ngSanitize","ngMaterial"]).config(["$routeProvider",function(e){e.when("/",{templateUrl:"views/main.html",controller:"MainCtrl",controllerAs:"Main"}).otherwise({redirectTo:"/"})}]),angular.module("ssorApp").controller("MainCtrl",function(){});
